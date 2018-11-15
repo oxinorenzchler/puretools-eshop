dea678ab66ff5b5301ef8c8ecb25547afcdf4512
oxinorenzchler <oxino.renzchler@gmail.com>
2018-11-15 08:33:55 +0800

    save session to delete and added image

---
+++ lib/controllers/sessiontodelete.php
@@ -0,0 +1,19 @@
+prod
+<?php unset($_SESSION['category_id']); ?>
+<?php unset($_SESSION['searchinput']); ?>
+
+catalog
+<?php unset($_SESSION['productID']); ?>
+<?php unset($_SESSION['category_id']); ?>
+<?php unset($_SESSION['filter']); ?>
+
+index
+<?php unset($_SESSION['productID']); ?>
+
+search
+<?php unset($_SESSION['productID']); ?>
+<?php unset($_SESSION['category_id']); ?>
+<?php unset($_SESSION['filter']); ?>
+
+category
+<?php unset($_SESSION['productID']); ?>
